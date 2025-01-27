9eeef98a9554116c750d21a4d0fbc7faca85c14d
jukle <julien.kle02@gmail.com>
2025-01-27 20:19:55 +0100

    Fix

---
+++ user-service/src/shared/utils/api-enums.ts
@@ -0,0 +1,15 @@
+export enum UserRoles {
+    super_admin = 'super_admin',
+    admin = 'admin',
+    staff = 'staff',
+    student = 'student',
+    pending = 'pending',
+}
+
+export enum RoleWeight {
+    super_admin,
+    admin,
+    staff,
+    student,
+    pending,
+}
