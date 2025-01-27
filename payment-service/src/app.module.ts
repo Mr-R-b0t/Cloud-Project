e50ac4a44907246ba0287170108277fbe02d80e6
Edran0111 <82849327+Edran0111@users.noreply.github.com>
2025-01-27 14:13:33 +0100

    Basic setup for functionnality

--- payment-service/src/app.module.ts
+++ payment-service/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PaymentService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 import { PaymentsController } from './payments/payments.controller';
 import { PaymentsService } from './payments/payments.service';
@@ -8,6 +8,6 @@ import { PaymentsService } from './payments/payments.service';
 @Module({
   imports: [PaymentsModule],
   controllers: [AppController, PaymentsController],
-  providers: [AppService, PaymentsService],
+  providers: [PaymentService, PaymentsService],
 })
 export class AppModule {}
--- payment-service/src/app.service.ts
+++ payment-service/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class PaymentService {
+  private readonly stripe = new Stripe("sk_test_51QlrmzADAvlHx1odrFyfJ79IlTSAdz1m3gySvYUW0uRPRndgUcgeaaft7Um4VL6c5h91hPthfTNycVNufrFT5alp00bnKhqXVb", {
+    apiVersion: '2024-12-18.acacia',
+  });
+  
+  async topUpWallet(userId: number, amount: number) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: amount * 100, // Stripe uses cents
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    // Assume successful payment (handle webhook for real implementation)
+    //await this.PaymentService.updateWalletBalance(userId, amount);
+    return paymentIntent;
+  }
+
+
+  getHello(): string{
+    return "HELLO";
   }
 }
