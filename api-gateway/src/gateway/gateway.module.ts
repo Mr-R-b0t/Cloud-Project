277b8f3ade594913204c985b61da8093c3d1d27d
Cyb0nix <cedric.yoganathan@efrei.net>
2025-01-28 11:07:31 +0100

    gateway.module.ts

--- api-gateway/src/gateway/gateway.module.ts
+++ api-gateway/src/gateway/gateway.module.ts
@@ -1,54 +1,52 @@
-import {MiddlewareConsumer, Module} from '@nestjs/common';
-import {createProxyMiddleware} from "http-proxy-middleware";
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 @Module({})
-export class GatewayModule {
+export class GatewayModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
     consumer
         .apply(
             createProxyMiddleware({
-                target: 'http://localhost:3001', // User Service
+                target: 'http://localhost:3001/users', // User Service
                 changeOrigin: true,
             }),
         )
         .forRoutes('/users');
 
     consumer
-        .apply(
-            createProxyMiddleware({
-                target: 'http://localhost:3002', // Property Service
-                changeOrigin: true,
-            }),
-        )
-        .forRoutes('/properties');
+      .apply(
+        createProxyMiddleware({
+          target: 'http://localhost:3002/properties', // Property Service
+          changeOrigin: true,
+        }),
+      )
+      .forRoutes('/properties');
 
     consumer
-        .apply(
-            createProxyMiddleware({
-                target: 'http://localhost:3003', // Investment Service
-                changeOrigin: true,
-            }),
-        )
-        .forRoutes('/investements');
+      .apply(
+        createProxyMiddleware({
+          target: 'http://localhost:3003/investments', // Investment Service
+          changeOrigin: true,
+        }),
+      )
+      .forRoutes('/investments');
 
     consumer
-        .apply(
-            createProxyMiddleware({
-                target: 'http://localhost:3004', // Payment Service
-                changeOrigin: true,
-            }),
-        )
-        .forRoutes('/payments');
+      .apply(
+        createProxyMiddleware({
+          target: 'http://localhost:3004/payments', // Payment Service
+          changeOrigin: true,
+        }),
+      )
+      .forRoutes('/payments');
 
     consumer
-        .apply(
-            createProxyMiddleware({
-                target: 'http://localhost:3005', // Notification Service
-                changeOrigin: true,
-            }),
-        )
-        .forRoutes('/notifications');
-    }
-
-
+      .apply(
+        createProxyMiddleware({
+          target: 'http://localhost:3005/notifications', // Notification Service
+          changeOrigin: true,
+        }),
+      )
+      .forRoutes('/notifications');
+  }
 }
