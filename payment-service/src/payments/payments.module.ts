bc28ba57a19bae0eac0f6785e808e6ff3810235c
Edran0111 <82849327+Edran0111@users.noreply.github.com>
2025-01-29 20:47:29 +0100

    Add comments + erase entity

--- payment-service/src/payments/payments.module.ts
+++ payment-service/src/payments/payments.module.ts
@@ -1,18 +1,15 @@
 // payment-service/src/payments/payments.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { PaymentController } from './controller/payments.controller';
 import { PaymentService } from './service/payments.service';
-import { Payment } from './entity/payments.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Payment]),
     ConfigModule
   ],
   controllers: [PaymentController],
   providers: [PaymentService],
-  exports: [PaymentService, TypeOrmModule],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
