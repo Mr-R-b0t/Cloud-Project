21c6754893f4fdcef054e31643561b5e285c375c
Cyb0nix <cedric.yoganathan@efrei.net>
2025-03-02 19:12:51 +0100

    Updated deployments

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule.forRoot(),
     AuthModule,
-    GatewayModule,
   ],
   controllers: [],
   providers: [],
